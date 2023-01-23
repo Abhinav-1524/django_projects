11af34abcece7190a43cf9803d780fe01f466c67
Abhinav-1524 <karriabhinav15@gmail.com>
2023-01-23 17:34:20 +0530

    poll site

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -17,9 +17,14 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
+
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -45,10 +50,3 @@ def vote(request, question_id):
 
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
